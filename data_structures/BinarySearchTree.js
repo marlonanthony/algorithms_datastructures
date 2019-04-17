abd50dd6d558ba07e785e6cf1f170468146b99ec
borgatajunky <mad1083@gmail.com>
2019-04-17 01:47:28 -0400

    BFS and DFS

--- data_structures/BinarySearchTree.js
+++ data_structures/BinarySearchTree.js
@@ -53,15 +53,63 @@ class BinarySearchTree {
     }
     return current 
   }
+
+  BFS() {
+    let node = this.root,
+          data = [],
+          queue = []
+          
+    queue.push(node) 
+    while(queue.length) {
+      node = queue.shift()
+      data.push(node.value) 
+      node.left && queue.push(node.left) 
+      node.right && queue.push(node.right) 
+    }
+    return data 
+  }
+
+  DFSPreOrder() {
+    const data = []
+    function traverse(node) {
+      data.push(node.value) 
+      node.left && traverse(node.left) 
+      node.right && traverse(node.right) 
+    }
+    traverse(this.root) 
+    return data 
+  }
+
+  DFSPostOrder(){
+    const data = []
+    function traverse(node) {
+      node.left && traverse(node.left) 
+      node.right && traverse(node.right) 
+      data.push(node.value) 
+    }
+    traverse(this.root) 
+    return data 
+  }
+
+  DFSInOrder() {
+    const data = []
+    function traverse(node) {
+      node.left && traverse(node.left) 
+      data.push(node.value) 
+      node.right && traverse(node.right) 
+    }
+    traverse(this.root) 
+    return data 
+  }
 }
 
 const tree = new BinarySearchTree() 
 tree.insert(10)
-tree.insert(8)
+tree.insert(6)
+tree.insert(15)
 tree.insert(20)
-tree.insert(19)
-tree.insert(4)
-tree.insert(9)
-tree.insert(18)
-console.log(tree.find(19))
-console.log(tree)
+tree.insert(3)
+tree.insert(8)
+console.log(tree.DFSPreOrder())
+console.log(tree.DFSPostOrder())
+console.log(tree.DFSInOrder())
