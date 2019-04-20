6b04411d1f60f805618678baf79a0e98fbdede28
borgatajunky <mad1083@gmail.com>
2019-04-20 02:02:52 -0400

    reverse array in place

---
+++ interview_questions/ReverseArrayInPlace.js
@@ -0,0 +1,17 @@
+function reverseArray(arr) {
+  let front = 0, 
+      back = arr.length - 1
+  
+  while(front < back) {
+    swap(arr, front, back) 
+    front++
+    back--
+  }
+  return arr
+}
+
+function swap(arr, front, back) {
+  [ arr[front], arr[back] ] = [ arr[back], arr[front] ]
+}
+
+console.log(reverseArray([1,2,3,4,5,6,7,8,9,10]))
