4b75e4950fd084008ca9a6724c21d4e40cc6bb10
borgatajunky <mad1083@gmail.com>
2019-04-21 00:23:18 -0400

    remove duplicates of array in place

---
+++ interview_questions/RemoveDuplicatesOfArray.js
@@ -0,0 +1,15 @@
+function removeDuplicates(nums) {
+  let n = 0,
+      c = 1
+  while(c < nums.length) {
+    if(nums[c] === nums[n]){
+      c++
+    } else {
+      n++
+      nums[n] = nums[c]
+    }
+  }
+  return n + 1
+}
+
+console.log(removeDuplicates([1,1,2]))
