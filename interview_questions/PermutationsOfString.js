2a42346e3f4e4b3428b0bbe891297d770f432e8f
borgatajunky <mad1083@gmail.com>
2019-04-26 00:00:29 -0400

    permutation answer 2

--- interview_questions/PermutationsOfString.js
+++ interview_questions/PermutationsOfString.js
@@ -1,22 +1,46 @@
 function permutationsOfString(string) {
-    function recur(string, prefix) {
-        if (string.length === 0) {
-            return [prefix];
-        } else {
-            let out = [];
-            for (let i = 0; i < string.length; i++) {
-                let pre = string.substring(0, i);
-                let post = string.substring(i + 1);
-                out = out.concat(recur(pre + post, string[i] + prefix));
-            }
-            return out;
-        }
+  const arr = string.split('')
+  let permutations = [] 
+
+  function swap(a,b) {
+    // console.log(a,b)
+    let temp = arr[a] 
+    arr[a] = arr[b] 
+    arr[b] = temp 
+  }
+
+  function generate(n) {
+    if(n === 1) {
+      console.log(permutations = permutations.concat(arr.join('')))
+    } else {
+      for(let i = 0; i !== n; i++) {
+        generate(n - 1) 
+        swap(n % 2 ? 0 : i, n - 1) 
+      }
     }
-    const distinct = {};
-    recur(string, "").forEach(function(result) {
-        distinct[result] = true;
-    });
-    return Object.keys(distinct);
+  }
+  generate(arr.length) 
+  return permutations.join('')
+
+    // function recur(string, prefix) {
+    //     if (string.length === 0) {
+    //         return [prefix];
+    //     } else {
+    //         let out = [];
+    //         for (let i = 0; i < string.length; i++) {
+    //             let pre = string.substring(0, i);
+    //             let post = string.substring(i + 1);
+    //             out = out.concat(recur(pre + post, string[i] + prefix))
+    //             console.log(out) 
+    //         }
+    //         return out;
+    //     }
+    // }
+    // const distinct = {};
+    // recur(string, "").forEach(function(result) {
+    //     distinct[result] = true;
+    // });
+    // return Object.keys(distinct);
 }
 
-console.log(permutationsOfString('abc'))
+console.log(permutationsOfString('marlon'))
