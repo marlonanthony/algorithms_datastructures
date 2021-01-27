f3cd6abf693de1f44f3ade4a594090c0e1e6fcd9
marlonanthony <mad1083@gmail.com>
2021-01-26 21:34:39 -0500

    permutationsOfString

--- interview_questions/PermutationsOfString.js
+++ interview_questions/PermutationsOfString.js
@@ -1,56 +1,9 @@
-function permutationsOfString(string) {
-
-  function recur(string, prefix) {
-      if (string.length === 0) {
-          return [prefix];
-      } else {
-          let out = [];
-          for (let i = 0; i < string.length; i++) {
-              let pre = string.substring(0, i);
-              let post = string.substring(i + 1);
-              out = out.concat(recur(pre + post, string[i] + prefix))
-              console.log(out) 
-          }
-          return out;
-      }
-  }
-  const distinct = {};
-  recur(string, "").forEach(function(result) {
-      distinct[result] = true
-  })
-  return Object.keys(distinct)
+function permutationsOfString(str, prefix='', data=[]){
+    let n = str.length 
+    if (n === 0) return data.push(prefix)
+    for (let i = 0; i < n; i++){
+        permutationsOfString(str.substring(0, i) + str.substring(i+1, n), prefix + str[i],  data)
+    }
+    return data
 }
-
-permutationsOfString('marlon')
-
-
-
-
-
-
-
-
-// function permutationsOfString(string) {
-//   const arr = string.split('')
-//   let permutations = [] 
-
-//   function swap(a,b) {
-//     console.log(a,b)
-//     let temp = arr[a] 
-//     arr[a] = arr[b] 
-//     arr[b] = temp 
-//   }
-
-//   function generate(n) {
-//     if(n === 1) {
-//       console.log(permutations = permutations.concat(arr.join('')))
-//     } else {
-//       for(let i = 0; i !== n; i++) {
-//         generate(n - 1) 
-//         swap(n % 2 ? 0 : i, n - 1) 
-//       }
-//     }
-//   }
-//   generate(arr.length) 
-//   return permutations.join('')
-// permutationsOfString('marlon')
+console.log(permutationsOfString('abc'))
